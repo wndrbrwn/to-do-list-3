ac0dde12e87227f1a8259547726f1c92274554e8
h662 <h662hong@gmail.com>
2024-05-29 13:43:42 +0900

    투두 데이터 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import { Flex } from "@chakra-ui/react";
-import { FC } from "react";
+import { FC, useState } from "react";
 import CreateTodo from "./components/CreateTodo";
 import TodoList from "./components/TodoList";
 
+const sampleData: ITodo[] = [
+  {
+    id: 1,
+    content: "🏃‍♀️ 달리기",
+    isDone: false,
+  },
+  {
+    id: 2,
+    content: "🎈 풍선구매",
+    isDone: false,
+  },
+  {
+    id: 3,
+    content: "🧹 청소하기",
+    isDone: false,
+  },
+];
+
 const App: FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>(sampleData);
+
   return (
     <Flex flexDir="column" minH="100vh">
       <CreateTodo />
-      <TodoList />
+      <TodoList todos={todos} />
     </Flex>
   );
 };
--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -2,11 +2,15 @@ import { Button, Flex, Text } from "@chakra-ui/react";
 import { FC } from "react";
 import { FiEdit3, FiTrash2 } from "react-icons/fi";
 
-const TodoCard: FC = () => {
+interface TodoCard {
+  todo: ITodo;
+}
+
+const TodoCard: FC<TodoCard> = ({ todo }) => {
   return (
     <Flex bgColor="white" px={4} py={2} rounded="lg" gap={1}>
       <Text fontSize={20} w={48} isTruncated={true}>
-        🏃‍♀️ 달리기
+        {todo.content}
       </Text>
       <Button colorScheme="blue">
         <FiEdit3 />
